1d5d3dd2b408f21ecf7d3fe0a550c9a9c3e6f1c7
Federico Bareilles <fede@libertad.fcaglp.unlp.edu.ar>
2021-06-29 03:06:05 -0300

    Fix telnet port

--- script/compare_time.sh
+++ script/compare_time.sh
@@ -3,4 +3,4 @@
 # ./compare_time.sh 10.10.10.160
 # ./compare_time.sh 192.168.22.241
 
-(date +%s;echo -e 'time\r\n'|nc -w1 $1 5002 )|awk '{if(NR==1){a=$1}} END{printf"%d %d %d\n", a,$1,a-$1;}'
+(date +%s;echo -e 'time\r\n'|nc -w1 $1 502 )|awk '{if(NR==1){a=$1}} END{printf"%d %d %d\n", a,$1,a-$1;}'
